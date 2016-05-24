ebc710798b9e1c57855a533cf95a0c25efbbe9a4
abaugus <abhishekbind2013@gmail.com>
2016-05-24 14:13:35 +0530

    debug22

--- maps.js
+++ maps.js
@@ -1,5 +1,79 @@
+var map;
+
+function getCoor() {
+  return coordinates;
+}
+
+function handle_files(files) {
+  for (i = 0; i < files.length; i++) {
+    file = files[i];
+    console.log(file);
+    var reader = new FileReader();
+    ret = [];
+    reader.onload = function(e) {
+      console.log(e.target.result);
+      var lines = e.target.result.split('\n');
+      var bounds = new google.maps.LatLngBounds();
+      for (var line = 0; line < lines.length; line++) {
+
+        var point = lines[line].split(',');
+
+        var count = 0;
+        var coordinates = [];
+        for (var dua = 0; dua < point.length; dua++) {
+          if (dua % 2 == 0) {
+            var latitude = point[dua];
+          } else {
+            var longitude = point[dua];
+            coordinates[count] = new google.maps.LatLng(latitude, longitude);
+            console.log(coordinates[count]);
+            count++;
+          }
+        }
+        var myTrip = [];
+        for (var c = 0; c < coordinates.length; c++) {
+          myTrip.push(coordinates[c]);
+          bounds.extend(coordinates[c]);
+        }
+        map.fitBounds(bounds);
+
+        //console.log(myTrip);
+        var flightPath = new google.maps.Polyline({
+          path: myTrip,
+          strokeColor: "#0000FF",
+          strokeOpacity: 0.8,
+          strokeWeight: 8
+        });
+
+        flightPath.setMap(map);
+
+      }
+
+    }
+    reader.onerror = function(stuff) {
+      console.log("error", stuff);
+      console.log(stuff.getMessage());
+    }
+    reader.readAsText(file); //readAsdataURL
+  }
+
+}
+
+function initialize() {
+
+  var mapProp = {
+
+    center: new google.maps.LatLng(8.611911, 41.146056),
+    zoom: 6,
+    mapTypeId: google.maps.MapTypeId.ROADMAP
+  };
+
+  map = new google.maps.Map(document.getElementById("googleMap"), mapProp);
+}
+
+google.maps.event.addDomListener(window, 'load', initialize);
 //var txtFile = "/Users/abhishekbind/Documents/nearbuy/Phase1/web/webappmap/test.txt";
-var txtFile = "test.txt";
+/*var txtFile = "test.txt";
 var file = new File(txtFile);
 
 file.open("r"); // open file with read access
@@ -10,7 +84,7 @@ while (!file.eof) {
 }
 file.close();
 window.alert(str);
-
+*/
 /*results = Papa.parse(file, {
     delimiter: ",",
     header: true,
