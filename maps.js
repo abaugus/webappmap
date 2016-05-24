caeb61ae6e1b0dfbf3d1aa5ab1511b9d9dd7f5d4
abaugus <abhishekbind2013@gmail.com>
2016-05-24 15:22:01 +0530

    marking on goole maps10

--- maps.js
+++ maps.js
@@ -38,27 +38,28 @@ function handle_files(files) {
                     bounds.extend(coordinates[c]);
                 }
                 map.fitBounds(bounds);
-                */var locations = [
-      ['Bondi Beach', -33.890542, 151.274856, 4],
-      ['Coogee Beach', -33.923036, 151.259052, 5],
-      ['Cronulla Beach', -34.028249, 151.157507, 3],
-      ['Manly Beach', -33.80010128657071, 151.28747820854187, 2],
-      ['Maroubra Beach', -33.950198, 151.259302, 1]
-    ];
-                for (i = 0; i < locations.length; i++) {  
-        windows.alert(locations);
-      marker = new google.maps.Marker({
-        position: new google.maps.LatLng(locations[i][1], locations[i][2]),
-        map: map
-      });
+                */
+                var locations = [
+                    ['Bondi Beach', -33.890542, 151.274856, 4],
+                    ['Coogee Beach', -33.923036, 151.259052, 5],
+                    ['Cronulla Beach', -34.028249, 151.157507, 3],
+                    ['Manly Beach', -33.80010128657071, 151.28747820854187, 2],
+                    ['Maroubra Beach', -33.950198, 151.259302, 1]
+                ];
+                for (i = 0; i < locations.length; i++) {
+                    windows.alert(locations);
+                    marker = new google.maps.Marker({
+                        position: new google.maps.LatLng(locations[i][1], locations[i][2]),
+                        map: map
+                    });
 
-      google.maps.event.addListener(marker, 'click', (function(marker, i) {
-        return function() {
-          infowindow.setContent(locations[i][0]);
-          infowindow.open(map, marker);
-        }
-      })(marker, i));
-    }
+                    google.maps.event.addListener(marker, 'click', (function(marker, i) {
+                        return function() {
+                            infowindow.setContent(locations[i][0]);
+                            infowindow.open(map, marker);
+                        }
+                    })(marker, i));
+                }
             }
 
         }
