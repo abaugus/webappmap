02e72cd9ef363847ee504d02a1a90a7f0bed7ee6
abaugus <abhishekbind2013@gmail.com>
2016-05-24 15:09:20 +0530

    marking on goole maps7

--- maps.js
+++ maps.js
@@ -38,21 +38,26 @@ function handle_files(files) {
                     bounds.extend(coordinates[c]);
                 }
                 map.fitBounds(bounds);
+                var locations = [
+      ['Bondi Beach', -33.890542, 151.274856, 4],
+      ['Coogee Beach', -33.923036, 151.259052, 5],
+      ['Cronulla Beach', -34.028249, 151.157507, 3],
+      ['Manly Beach', -33.80010128657071, 151.28747820854187, 2],
+      ['Maroubra Beach', -33.950198, 151.259302, 1]
+    ];
+                for (i = 0; i < locations.length; i++) {  
+      marker = new google.maps.Marker({
+        position: new google.maps.LatLng(locations[i][1], locations[i][2]),
+        map: map
+      });
 
-                for (i = 0; i < coordinates.length; i++) {
-                    window.alert(coordinates[i]);
-                    marker = new google.maps.Marker({
-                        position: new google.maps.LatLng(coordinates[i][0], coordinates[i][1]),
-                        map: map
-                    });
-
-                    google.maps.event.addListener(marker, 'click', (function(marker, i) {
-                        return function() {
-                            infowindow.open(map, marker);
-                        }
-                    })(marker, i));
-                }
-
+      google.maps.event.addListener(marker, 'click', (function(marker, i) {
+        return function() {
+          infowindow.setContent(locations[i][0]);
+          infowindow.open(map, marker);
+        }
+      })(marker, i));
+    }
             }
 
         }
