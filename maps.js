56a9c094ad62f720d9dbfbd2577526c67dc9760d
abaugus <abhishekbind2013@gmail.com>
2016-05-24 11:07:47 +0530

    adding maps.js 1

---
+++ maps.js
@@ -0,0 +1,24 @@
+var address = 'New Delhi, India';
+
+   var map = new google.maps.Map(document.getElementById('map'), { 
+       mapTypeId: google.maps.MapTypeId.TERRAIN,
+       zoom: 6
+   });
+
+   var geocoder = new google.maps.Geocoder();
+
+   geocoder.geocode({
+      'address': address
+   }, 
+   function(results, status) {
+      if(status == google.maps.GeocoderStatus.OK) {
+         new google.maps.Marker({
+            position: results[0].geometry.location,
+            map: map
+         });
+         map.setCenter(results[0].geometry.location);
+      }
+      else {
+         // Google couldn't geocode this request. Handle appropriately.
+      }
+   });
