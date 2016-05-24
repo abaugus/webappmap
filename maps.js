17286a55571ff83a158f700e6431cf09e5c860cb
abaugus <abhishekbind2013@gmail.com>
2016-05-24 14:43:28 +0530

    marking on goole maps

--- maps.js
+++ maps.js
@@ -1,74 +1,78 @@
 var map;
 
 function getCoor() {
-  return coordinates;
+    return coordinates;
 }
 
 function handle_files(files) {
-  for (i = 0; i < files.length; i++) {
-    file = files[i];
-    console.log(file);
-    var reader = new FileReader();
-    ret = [];
-    reader.onload = function(e) {
-      console.log(e.target.result);
-      var lines = e.target.result.split('\n');
-      var bounds = new google.maps.LatLngBounds();
-      for (var line = 0; line < lines.length; line++) {
+    for (i = 0; i < files.length; i++) {
+        file = files[i];
+        console.log(file);
+        var reader = new FileReader();
+        ret = [];
+        reader.onload = function(e) {
+            console.log(e.target.result);
+            var lines = e.target.result.split('\n');
+            var bounds = new google.maps.LatLngBounds();
+            for (var line = 0; line < lines.length; line++) {
 
-        var point = lines[line].split(',');
+                var point = lines[line].split(',');
 
-        var count = 0;
-        var coordinates = [];
-        for (var dua = 0; dua < point.length; dua++) {
-          if (dua % 2 == 0) {
-            var latitude = point[dua];
-          } else {
-            var longitude = point[dua];
-            coordinates[count] = new google.maps.LatLng(latitude, longitude);
-            console.log(coordinates[count]);
-            count++;
-          }
+                var count = 0;
+                var coordinates = [];
+                for (var dua = 0; dua < point.length; dua++) {
+                    if (dua % 2 == 0) {
+                        var latitude = point[dua];
+                    } else {
+                        var longitude = point[dua];
+                        coordinates[count] = new google.maps.LatLng(latitude, longitude);
+                        console.log(coordinates[count]);
+                        count++;
+                    }
+                }
+                var locations = [];
+                for (var c = 0; c < coordinates.length; c++) {
+                    locations.push(coordinates[c]);
+                    bounds.extend(coordinates[c]);
+                }
+                map.fitBounds(bounds);
+
+                for (i = 0; i < locations.length; i++) {
+                    marker = new google.maps.Marker({
+                        position: new google.maps.LatLng(locations[i][1], locations[i][2]),
+                        map: map
+                    });
+
+                    google.maps.event.addListener(marker, 'click', (function(marker, i) {
+                        return function() {
+                            infowindow.setContent(locations[i][0]);
+                            infowindow.open(map, marker);
+                        }
+                    })(marker, i));
+                }
+
+            }
+
         }
-        var myTrip = [];
-        for (var c = 0; c < coordinates.length; c++) {
-          myTrip.push(coordinates[c]);
-          bounds.extend(coordinates[c]);
+        reader.onerror = function(stuff) {
+            console.log("error", stuff);
+            console.log(stuff.getMessage());
         }
-        map.fitBounds(bounds);
-
-        //console.log(myTrip);
-        var flightPath = new google.maps.Polyline({
-          path: myTrip,
-          strokeColor: "#0000FF",
-          strokeOpacity: 0.8,
-          strokeWeight: 8
-        });
-
-        flightPath.setMap(map);
-
-      }
-
+        reader.readAsText(file); //readAsdataURL
     }
-    reader.onerror = function(stuff) {
-      console.log("error", stuff);
-      console.log(stuff.getMessage());
-    }
-    reader.readAsText(file); //readAsdataURL
-  }
 
 }
 
 function initialize() {
 
-  var mapProp = {
+    var mapProp = {
 
-    center: new google.maps.LatLng(28.636523,77.224962),
-    zoom: 6,
-    mapTypeId: google.maps.MapTypeId.ROADMAP
-  };
+        center: new google.maps.LatLng(28.636523, 77.224962),
+        zoom: 6,
+        mapTypeId: google.maps.MapTypeId.ROADMAP
+    };
 
-  map = new google.maps.Map(document.getElementById("googleMap"), mapProp);
+    map = new google.maps.Map(document.getElementById("googleMap"), mapProp);
 }
 
 google.maps.event.addDomListener(window, 'load', initialize);
@@ -91,7 +95,8 @@ window.alert(str);
     dynamicTyping: true
 });
 console.log(results);
-*//*
+*/
+/*
 InputStream instream = new FileInputStream("data/source.csv");
 InputStreamReader inputreader = new InputStreamReader(instream);
 BufferedReader reader= new BufferedReader(inputreader);
