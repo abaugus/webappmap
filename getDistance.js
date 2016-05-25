6abddf45f31bfb0309712f4b72b3bcc603f9b61c
abaugus <abhishekbind2013@gmail.com>
2016-05-25 13:26:50 +0530

    added radius feature2

--- getDistance.js
+++ getDistance.js
@@ -2,10 +2,11 @@
 	Shows the distance between two LatLng
 	->Add arguments
 */
-function Distance(){
-	var p1 = new google.maps.LatLng(40.718119, -73.995667);
-	var p2 = new google.maps.LatLng(51.499981, -0.125313);
+function Distance(p1,p2){
+	//var p1 = new google.maps.LatLng(40.718119, -73.995667);
+	//var p2 = new google.maps.LatLng(51.499981, -0.125313);
 	var dist = google.maps.geometry.spherical.computeDistanceBetween(p1,p2);
-	window.alert(dist);
+	return dist;
+	//window.alert(dist);
 }
 
