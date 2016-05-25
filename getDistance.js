faab28cb3b48ade36f4850c5ca7b8afeb487e10e
abaugus <abhishekbind2013@gmail.com>
2016-05-25 14:32:49 +0530

    removed bower components

--- getDistance.js
+++ getDistance.js
@@ -1,12 +1,10 @@
 /* 
 	Shows the distance between two LatLng
-	->Add arguments
 */
 function Distance(p1,p2){
 	//var p1 = new google.maps.LatLng(40.718119, -73.995667);
 	//var p2 = new google.maps.LatLng(51.499981, -0.125313);
 	var dist = google.maps.geometry.spherical.computeDistanceBetween(p1,p2);
 	return dist;
-	//window.alert(dist);
 }
 
