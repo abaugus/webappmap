38b73f30d568ba3229c870b6148446535d79b14f
abaugus <abhishekbind2013@gmail.com>
2016-05-26 11:28:10 +0530

    debug unmarking of markers18

--- maps.js
+++ maps.js
@@ -52,7 +52,8 @@ function handle_files(files) {
                         var longitude = point[dua];
                         coordinates[count] = new google.maps.LatLng(latitude, longitude);
                         console.log(coordinates[count]);
-                        window.alert(Distance(coordinates[count],center),getRadius());
+                        window.alert(Distance(coordinates[count],center));
+                        window.alert(getRadius());
                         if(Distance(coordinates[count],center)<=getRadius())
                         {
                             marker = new google.maps.Marker({
