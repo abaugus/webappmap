a419eb0eb5871b7f4983691ca558787faec9f371
abaugus <abhishekbind2013@gmail.com>
2016-05-26 11:21:18 +0530

    debug unmarking of markers13

--- getDistance.js
+++ getDistance.js
@@ -4,7 +4,7 @@
 function Distance(p1,p2){
 	//var p1 = new google.maps.LatLng(40.718119, -73.995667);
 	//var p2 = new google.maps.LatLng(51.499981, -0.125313);
-	var dist = google.maps.geometry.spherical.computeDistanceBetween(p1,p2)/1000;
+	var dist = google.maps.geometry.spherical.computeDistanceBetween(p1,p2);
 	return dist;
 }
 
