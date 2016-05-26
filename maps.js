81202ac7593643c63ca04e60c54789ae863b9598
abaugus <abhishekbind2013@gmail.com>
2016-05-26 12:18:03 +0530

    umnarking markers

--- maps.js
+++ maps.js
@@ -23,8 +23,9 @@ function handle_files(files) {
     //clearMarkers();
     window.alert("handle_files");
     var center = new google.maps.LatLng(28.636523, 77.224962);
-    //for (i = 0; i < files.length; i++) {
-    {   window.alert("insideloop");
+    for (i = 0; i < files.length; i++) {
+    {   
+        //window.alert("insideloop");
         file = files[i];
         console.log(file);
         var reader = new FileReader();
@@ -44,8 +45,8 @@ function handle_files(files) {
                 var marker, i;
                 window.alert("went");
                         
-                //if(check==0)
-                //{
+                if(check==0)
+                {
                     for (var dua = 0; dua < point.length; dua++) {
                     if (dua % 2 == 0) {
                         var latitude = point[dua];
@@ -54,9 +55,9 @@ function handle_files(files) {
                         var longitude = point[dua];
                         coordinates[count] = new google.maps.LatLng(latitude, longitude);
                         console.log(coordinates[count]);
-                        window.alert(Distance(coordinates[count],center));
-                        window.alert(getRadius());
-                        if(Distance(coordinates[count],center)<=getRadius())
+                        //window.alert(Distance(coordinates[count],center));
+                        //window.alert(getRadius());
+                        //if(Distance(coordinates[count],center)<=getRadius())
                         {
                             marker = new google.maps.Marker({
                             position: new google.maps.LatLng(latitude, longitude),
@@ -72,27 +73,22 @@ function handle_files(files) {
                             google.maps.event.addListener(marker,)
                             })(marker, i));
                         }
-                        else
-                        {
-                            window.alert("else");
-                            if(count<markers.length)
-                            {
-                                window.alert("elsesetmap");
-                                markers[count].setMap(null);  
-                            }
-                        }
                         count++;
                     }
-                    ++check;
+                    check=1;
                 }
-                /*else
+                
+                for(var c=0;c<markers.length;c++)
                 {
-                    for(var dua=0; dua<markers.length;dua++)
+                    if(Distance(coordinates[c],center)<=getRadius())
                     {
-                        if()
+                        markers[c].setMap(map);  
+                    }
+                    else
+                    {
+                        markers[c].setMap(null);
                     }
                 }
-                }*/
                 
                 var locations = [];
                 for (var c = 0; c < coordinates.length; c++) {
