a6fc19250b1fd3d34afae4182cfc98eb5252a719
abaugus <abhishekbind2013@gmail.com>
2016-05-26 14:21:31 +0530

    added resetfile2

--- maps.js
+++ maps.js
@@ -83,6 +83,7 @@ function handle_files(files) {
                     }
                     else
                     {
+                        window.alert("asd");
                         testCircle();            
                     }
 
